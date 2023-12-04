2ded0fcfce130132926941a302809852b2efa039
cesarjakub <list>
2023-12-04 12:31:13 +0100

    feat: socket logic

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
+@socketio.on('message')
+def handle_message(message):
+    print('Received message:', message)
+    socketio.emit('message', message)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
